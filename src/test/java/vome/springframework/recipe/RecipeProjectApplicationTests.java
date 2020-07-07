dffec618dc923ec6f23a28211b53f9d851b437bf
mv343203 <mvome@outlook.com>
2020-07-07 13:16:16 -0400

    Created a JUnit test and also ran already created integration test for Spring.

--- src/test/java/vome/springframework/recipe/RecipeProjectApplicationTests.java
+++ src/test/java/vome/springframework/recipe/RecipeProjectApplicationTests.java
@@ -1,8 +1,13 @@
 package vome.springframework.recipe;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+//integration test
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class RecipeProjectApplicationTests {
 
